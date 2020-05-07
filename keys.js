5a8e1b84b2a10cb05691cce9659634bf6ebda8dd
arturogaal98 <arturomcc@gmail.com>
2020-05-07 16:20:54 -0500

    loginnode

---
+++ keys.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+    database: {
+        connectionLimit: 10,
+        host: 'remotemysql.com',
+        user: 'AJXSUvsZXC',
+        password: 'lbl92cE5KO',
+        database: 'AJXSUvsZXC'
+    }
+
+};
